984febeec8a3576bdbb02504a4490793e4af1752
Vinay Prasad <vinayprasad2545@gmail.com>
2024-12-16 11:44:08 +0530

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-
+import Header from "./Header"
 function App() {
   return (
-    <>
-    <h1>Hope..!</h1>
-    <h4>Web App</h4>
-    </>
+    <Header/>
   )
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <header>
+            <div></div>
+            <ul>
+                <li>Home</li>
+                <li>Contribute</li>
+                <li>Contact Us</li>
+            </ul>
+        </header>
+    )
+}
