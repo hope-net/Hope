a4bc1fa58e8b2a9a8789d27ed5a6227471c8477d
Vinay Prasad <vinayprasad2545@gmail.com>
2024-12-16 11:58:28 +0530

    sample main component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Header from "./Header"
+import Main from "./Main"
 function App() {
   return (
+    <>
     <Header/>
+    <Main/>
+    </>
+
   )
 }
 
---
+++ src/Main.jsx
@@ -0,0 +1,11 @@
+export default function Main() {
+    return(
+        <main>
+            <h1>Find  Everything  in One Place</h1>
+            <h4>Web & Mobile App for KTU students</h4>
+            <h5>Syllabus | Notes | Question Paper | TextBooks | Videos |
+            GPA Calculator</h5>
+            <button>Explore</button>
+        </main>
+    )
+}
